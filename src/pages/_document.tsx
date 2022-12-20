10c1752d057e01f68715380058c787d13c4bda9b
kage1020 <4620087@ed.tus.ac.jp>
2022-12-20 13:57:41 +0900

    ✨ Add _document.tsx

---
+++ src/pages/_document.tsx
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+const MyApp = () => {
+  return (
+    <Html lang='ja'>
+      <Head></Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
+
+export default MyApp
