904de6e2c3f53d804bd40ac5ec4ed595dd2bb666
kage1020 <4620087@ed.tus.ac.jp>
2023-05-08 21:29:43 +0900

    🐛 fix absolute path import

--- app/api/auth/[...nextauth].ts
+++ app/api/auth/[...nextauth].ts
@@ -3,7 +3,7 @@ import NextAuth from 'next-auth'
 import CredentialsProvider from 'next-auth/providers/credentials'
 import GoogleProvider from 'next-auth/providers/google'
 
-import prisma from '@/libs/prisma'
+import prisma from '@/_libs/prisma'
 
 import type { NextAuthOptions } from 'next-auth'
 
